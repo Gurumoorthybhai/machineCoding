b49cd913ebaba26f77bfe5594aaf3c2785a1b5d7
Gurumoorthy M <gurunaveen22@gmail.com>
2024-02-13 23:58:22 +0530

    Update textfield keyboard throttle & compare the performance change

--- 5.Autocomplete/Debouncing/script.js
+++ 5.Autocomplete/Debouncing/script.js
@@ -1,14 +1,24 @@
 const inputField = document.getElementById('input-field');
-
+const normal = document.getElementById('normal');
+const debounce = document.getElementById('debounce');
+const throttle = document.getElementById('throttle');
 
 inputField.addEventListener('keyup', (e) => {
     let value = e.target.value;
-    handleKeyUp(value)
+    // handleKeyUp(value)
+    // handleWithDebounce(value)
+    handleWithThrottle(value);
 });
 
-const handleKeyUp = DebounceHandleKeyUp((text) => {
+const handleKeyUp = (text) => {
+    normal.textContent = text;
+    // console.log(' Key value -', text);
+};
+
+const handleWithDebounce = DebounceHandleKeyUp((text) => {
+    debounce.textContent = text;
     console.log(' Key value -', text);
-}, 1000)
+}, 1000);
 
 function DebounceHandleKeyUp(func, delay) {
 
@@ -24,3 +34,25 @@ function DebounceHandleKeyUp(func, delay) {
     }
 }
 
+
+const throttleHandleKeyUp = (cb, delay) => {
+
+    let timer;
+
+    return (...args) => {
+
+        if (timer) {
+
+            return;
+        }
+        cb.apply(this, args);
+        timer = setTimeout(() => {
+            timer = undefined;
+        }, delay);
+    }
+}
+
+const handleWithThrottle = throttleHandleKeyUp((text) => {
+    console.log(' throttle ', text);
+    throttle.textContent = text;
+}, 1000);
